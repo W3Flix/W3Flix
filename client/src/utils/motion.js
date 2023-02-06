cb9ba87ee6a7198a202f8faea3be77d58ff8605f
pranavcpawar <pranavcpawar@outlook.com>
2023-02-06 22:03:41 +0530

    initial version

---
+++ client/src/utils/motion.js
@@ -0,0 +1,21 @@
+export const navVariants = {
+    hidden: {
+      opacity: 0,
+			x:-50,
+			transition: {
+				type: 'string',
+				stiffness: 300,
+				damping: 140,
+			},
+    },
+		
+		show: {
+			opacity: 1,
+			x:0,
+			transition: {
+				type: 'spring',
+				stiffness: 80,
+				delay: 1,
+			},
+		},
+};
